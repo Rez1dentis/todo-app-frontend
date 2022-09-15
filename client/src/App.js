cbfe457e94b8cb10ef98f5a759e99b9ac5ac00eb
Ivan Kosenko <jean.kosenko@gmail.com>
2022-09-15 15:58:49 +0300

    release 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import TodoList from './components/TodoList/TodoList';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const [todoTitle, setTodoTitle] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (event) => {
+    if (event.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        },
+      ]);
+      setTodoTitle('');
+    }
+  };
+
   return (
-    <div className="App">
-      APP!!!
+    <div className="container">
+      <h1>Todo app</h1>
+
+      <div className="input-field">
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+          onKeyPress={addTodo}
+        />
+        <label>Введите текст</label>
+      </div>
+
+      <TodoList todos={todos} />
     </div>
   );
 }
-
-export default App;
